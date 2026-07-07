200870af80cc90aa1d42ac867c3c066c8f36e614
SHIZUKU_nanoda <116448951+noelfwfaw@users.noreply.github.com>
2026-07-07 10:31:13 +0800

    Merge remote-tracking branch 'upstream/master' into update

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -245,7 +245,7 @@ class GeneratedConfig:
     MaritimeEscort_Enable = True
 
     # Group `Coalition`
-    Coalition_Mode = 'hard'  # easy, normal, hard, sp
+    Coalition_Mode = 'tc3'  # tc1, tc2, tc3, sp, ex
     Coalition_Fleet = 'single'  # single, multi
 
     # Group `EventShop`
@@ -296,6 +296,7 @@ class GeneratedConfig:
     # Group `Meowfficer`
     Meowfficer_BuyAmount = 1
     Meowfficer_FortChoreMeowfficer = True
+    Meowfficer_OverflowCoins = -1
 
     # Group `MeowfficerTrain`
     MeowfficerTrain_Enable = False
@@ -409,7 +410,7 @@ class GeneratedConfig:
     # Group `PrivateQuarters`
     PrivateQuarters_BuyRoses = True
     PrivateQuarters_TargetInteract = True
-    PrivateQuarters_TargetShip = 'anchorage'  # anchorage, noshiro, sirius, new_jersey, taihou, aegir
+    PrivateQuarters_TargetShip = 'anchorage'  # anchorage, noshiro, sirius, new_jersey, taihou, aegir, nakhimov
 
     # Group `Daily`
     Daily_UseDailySkip = True
