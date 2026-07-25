be33d935502eb00378c287b41e9ca9f0608967cd
SHIZUKU_nanoda <116448951+noelfwfaw@users.noreply.github.com>
2026-07-25 11:50:58 +0800

    Merge remote-tracking branch 'upstream/master' into update

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -249,7 +249,7 @@ class GeneratedConfig:
     MaritimeEscort_Enable = True
 
     # Group `Coalition`
-    Coalition_Mode = 'tc3'  # tc1, tc2, tc3, sp, ex
+    Coalition_Mode = 'hard'  # easy, normal, hard, sp
     Coalition_Fleet = 'single'  # single, multi
 
     # Group `EventShop`
