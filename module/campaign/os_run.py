43decb9b9e6ba66a781c3f7e5d928ce379ad567e
SHIZUKU_nanoda <116448951+noelfwfaw@users.noreply.github.com>
2026-08-14 11:09:19 +0800

    Merge remote-tracking branch 'upstream/master' into update

--- module/campaign/os_run.py
+++ module/campaign/os_run.py
@@ -71,6 +71,9 @@ class OSCampaignRun(OSMapOperation):
             campaign.os_obscure()
         except ActionPointLimit:
             self.config.opsi_task_delay(ap_limit=True)
+            if self.config.is_task_enabled('OpsiHazard1Leveling') \
+                    and self.get_yellow_coins() > self.config.OS_CL1_YELLOW_COINS_PRESERVE:
+                self.config.task_call('OpsiHazard1Leveling')           
 
     def opsi_month_boss(self):
         if self.config.SERVER in ['tw']:
@@ -91,6 +94,9 @@ class OSCampaignRun(OSMapOperation):
             campaign.os_abyssal()
         except ActionPointLimit:
             self.config.opsi_task_delay(ap_limit=True)
+            if self.config.is_task_enabled('OpsiHazard1Leveling') \
+                    and self.get_yellow_coins() > self.config.OS_CL1_YELLOW_COINS_PRESERVE:
+                self.config.task_call('OpsiHazard1Leveling')
 
     def opsi_archive(self):
         try:
@@ -105,6 +111,9 @@ class OSCampaignRun(OSMapOperation):
             campaign.os_stronghold()
         except ActionPointLimit:
             self.config.opsi_task_delay(ap_limit=True)
+            if self.config.is_task_enabled('OpsiHazard1Leveling') \
+                    and self.get_yellow_coins() > self.config.OS_CL1_YELLOW_COINS_PRESERVE:
+                self.config.task_call('OpsiHazard1Leveling')
 
     def opsi_cross_month(self):
         campaign = self.load_campaign()
